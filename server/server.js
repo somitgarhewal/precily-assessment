3774f372996d68c03238bb182c8babeafa814f50
somitgarhewal <somitgarhewal@gmail.com>
2022-02-17 14:43:16 +0530

    add static server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 var cors = require('cors')
 var bodyparser = require('body-parser')
-
+const path = require('path');
 const router = require('./router')
 
 const app = express();
@@ -10,6 +10,12 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors())
 app.use(bodyparser.json())
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 mongoose.connect('mongodb+srv://somitdb:somitdbpassword@cluster0.in6qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useUnifiedTopology: true,
